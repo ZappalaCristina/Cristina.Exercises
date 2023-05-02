53909c895fc9e0c3e86cca6d42e8d8c937b526fe
Cristina Zappalà <zappala.cristina01@gmail.com>
2023-05-02 14:22:26 +0200

    PC fisso

---
+++ GeographicArea/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace GeographicArea
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Console.WriteLine("Hello World!");
+            Continent europe = new Continent("Europe");
+            europe.CreateNation("Italy");
+            europe.CreateRegion("Sicily");
+            europe.CreateProvince("Messina");
+            europe.CreateMunicipality("Giardini-Naxos");
+            //Console.WriteLine($" {}"); 
+            europe.ChangeMunicipality("Italy", "Sicily", "Catania", "Giardini-Naxos");
+        }
+    }
+}
